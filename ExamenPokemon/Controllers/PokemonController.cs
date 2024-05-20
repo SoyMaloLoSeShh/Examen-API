d5bac850e82ab10a13b970fe15b9c75f72637be2
SoyMaloLoSeShh <jose.nunez.villalobos@est.una.cr>
2024-05-20 12:22:03 -0600

    Examen

---
+++ ExamenPokemon/Controllers/PokemonController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyDearPokemon.Services.Pokemones;
+using MyDearPokemon.Services;
+
+namespace ExamenPokemon.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PokemonController : Controller
+{
+    private readonly PokemonServices _services;
+
+    public PokemonController(PokemonServices services)
+    {
+        _services = services;
+    }
+    [HttpGet("{PokeName}")]
+
+    public async Task<ActionResult<Pokemon>> Get(string PokemonName)
+    {
+        var Poke = await _services.GetPokemon(PokemonName);
+        return Ok(Poke);
+    }
+}
---
+++ MyDearPokemon/Servicios/PokemonServicios.cs
@@ -0,0 +1,29 @@
+﻿using MyDearPokemon.Services.Pokemones;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace MyDearPokemon.Services;
+public class PokemonServices
+{
+    private readonly HttpClient _httpClient;
+
+    public PokemonServices(HttpClient httpClient)
+    { _httpClient = httpClient; }
+
+    public async Task<Pokemon> GetPokemon(string PokemonName)
+    {
+        var response = await _httpClient.GetAsync($"https://pokeapi.co/api/v2/pokemon/{PokemonName}");
+        response.EnsureSuccessStatusCode();
+
+        var PokemonData = await response.Content.ReadFromJsonAsync<JsonElement>();
+
+        var Poke = new Pokemon
+        {
+            Name = PokemonName,
+            Type = PokemonData.GetProperty("types").EnumerateArray().First().GetProperty("type").GetProperty("name").GetString(),
+            URL = PokemonData.GetProperty("sprites").GetProperty("front_default").GetString(),
+            Moves = PokemonData.GetProperty("moves").EnumerateArray().Select(m => m.GetProperty("move").GetProperty("name").GetString()).ToList()
+        };
+        return Poke;
+    }
+}
